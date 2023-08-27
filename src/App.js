c2ac73ade8d3741bf5f1b72ad93fe9433ff3c8d4
michael ma <michaelma@wirelessprv-10-193-234-195.near.illinois.edu>
2023-08-27 16:34:36 -0500

    first

--- src/App.js
+++ src/App.js
@@ -4,20 +4,24 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> michaelm </h1>
+      hi im a software engineer @ <a href="https://basis.com"> basis technologies </a>
+      <h2> about </h2> 
+      i like algorithms, number theory, sushi, kickboxing, times new roman, lists, travelling, nine inch nails,
+      <br/>
+      program optimization, english history, 5 seconds of summer, megaman games, html without css, asyndetons
+      <br/>
+      <h2>additional info</h2>
+      mich.m11@outlook.com       
+      <br/>
+      <a href = '/RESUME2024.pdf' target = "_blank" >resume </a>
+      <br/>
+      <a href = "https://www.linkedin.com/in/michael--ma/">linkedin</a>
+      <br/>
+      <br/>
+      <footer>
+      last updated : 08/27/2023
+      </footer>
     </div>
   );
 }
